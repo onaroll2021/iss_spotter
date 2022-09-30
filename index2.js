bd2a31314e8c088d8711007bc56a1339ec788402
onaroll2021 <lukeli.onaroll@gmail.com>
2022-09-30 08:19:18 +0000

    update info aboutpromises

---
+++ index2.js
@@ -0,0 +1,19 @@
+const { nextISSTimesForMyLocation}  = require('./iss_promised');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
+ // fetch returns a promise object, call .then on its return value, and takes in a callback which accepts the response body
